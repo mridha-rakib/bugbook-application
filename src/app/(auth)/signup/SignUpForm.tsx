40810ba8f4748274d816c02d0ecfbee103054bbd
mridha-rakib <rakibmahmudmridha@gmail.com>
2024-11-10 11:15:11 +0600

    Loading button components and signup form

---
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import LoadingButton from "@/components/LoadingButton";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const SignUpForm = () => {
+  const form = useForm<SignUpValues>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" type="email" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <LoadingButton loading={isPending} type="submit" className="w-full">
+          Create account
+        </LoadingButton>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUpForm;
