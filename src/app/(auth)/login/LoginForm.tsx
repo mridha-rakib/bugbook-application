97eb6930cae213cebf30e63808647679ece18efd
mridha-rakib <rakibmahmudmridha@gmail.com>
2024-11-11 11:56:40 +0600

    login index page design

---
+++ src/app/(auth)/login/LoginForm.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import LoadingButton from "@/components/LoadingButton";
+import { PasswordInput } from "@/components/PasswordInput";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { loginSchema, LoginValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { login } from "./actions";
+
+export default function LoginForm() {}
--- src/app/(auth)/signup/SignUpForm.tsx
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -2,19 +2,26 @@
 
 import LoadingButton from "@/components/LoadingButton";
 import { PasswordInput } from "@/components/PasswordInput";
-import { signUpSchema, SignUpValues } from "@/lib/validation";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { signUp } from "./actions";
 
-const SignUpForm = () => {
+export default function SignUpForm() {
+  const [error, setError] = useState<string>();
+
+  const [isPending, startTransition] = useTransition();
+
   const form = useForm<SignUpValues>({
     resolver: zodResolver(signUpSchema),
     defaultValues: {
@@ -23,9 +30,19 @@ const SignUpForm = () => {
       password: "",
     },
   });
+
+  async function onSubmit(values: SignUpValues) {
+    setError(undefined);
+    startTransition(async () => {
+      const { error } = await signUp(values);
+      if (error) setError(error);
+    });
+  }
+
   return (
     <Form {...form}>
-      <form>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        {error && <p className="text-center text-destructive">{error}</p>}
         <FormField
           control={form.control}
           name="username"
@@ -35,6 +52,7 @@ const SignUpForm = () => {
               <FormControl>
                 <Input placeholder="Username" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -47,10 +65,10 @@ const SignUpForm = () => {
               <FormControl>
                 <Input placeholder="Email" type="email" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
-
         <FormField
           control={form.control}
           name="password"
@@ -60,16 +78,14 @@ const SignUpForm = () => {
               <FormControl>
                 <PasswordInput placeholder="Password" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
-
-        <LoadingButton type="submit" className="mt-3 w-full">
+        <LoadingButton loading={isPending} type="submit" className="w-full">
           Create account
         </LoadingButton>
       </form>
     </Form>
   );
-};
-
-export default SignUpForm;
+}
