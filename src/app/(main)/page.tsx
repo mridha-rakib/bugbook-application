e0b07118d1193a14e9c13d4b2ab0317c6743657a
mridha-rakib <rakibmahmudmridha@gmail.com>
2024-11-25 11:31:37 +0600

    fetch data using react query

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,22 +1,13 @@
 import PostEditor from "@/components/posts/editor/postEditor";
-import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import ForYourFeed from "./ForYourFeed";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany({
-    include: postDataInclude,
-    orderBy: { createdAt: "desc" },
-  });
-
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+        <ForYourFeed />
       </div>
       <TrendsSidebar />
     </main>
